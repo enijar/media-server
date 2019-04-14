6123cc9ee895198b61293bb43724859db613ab1c
Enijar <TheEnijar@gmail.com>
2019-04-14 21:34:06 +0100

    Removed

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const basicAuth = require('express-basic-auth');
 require('./bootstrap');
 
 const app = express();
@@ -9,12 +8,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, '..', '..', 'public')));
 app.use(bodyParser.urlencoded({extended: true}));
-
-app.use(basicAuth({
-    users: {
-        'admin': process.env.ADMIN_PASSWORD,
-    }
-}));
 
 require('./routes')(app);
 
