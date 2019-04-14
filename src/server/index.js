d0f53457f53cf6c120afbf0d64f4ffe6b0dea2a9
Enijar <TheEnijar@gmail.com>
2019-04-14 21:22:57 +0100

    Added

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+require('./bootstrap');
 
 const app = express();
 
@@ -8,9 +9,15 @@ app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, '..', '..', 'public')));
 app.use(bodyParser.urlencoded({extended: true}));
 
-require('./routes')(app);
+const basicAuth = require('express-basic-auth');
 
-require('./bootstrap');
+app.use(basicAuth({
+    users: {
+        'admin': process.env.ADMIN_PASSWORD,
+    }
+}));
+
+require('./routes')(app);
 
 app.listen(3000, () => {
     console.log(`Running http://localhost:3000`);
