27bf1bdacf1d4d4b4ac36c3ea3dd24a1ce449bfd
Enijar <TheEnijar@gmail.com>
2019-10-06 15:22:46 +0100

    Fixed video seeking issue in stream controller

--- src/server/controllers/StreamController.js
+++ src/server/controllers/StreamController.js
@@ -1,5 +1,7 @@
 const {Op} = require('sequelize');
 const torrentStream = require('torrent-stream');
+const pump = require('pump');
+const rangeParser = require('range-parser');
 const Movie = require('../models/Movie');
 const services = require('../services/index');
 
@@ -29,23 +31,34 @@ module.exports = async (req, res) => {
       return res.end();
     }
 
-    const headers = {
-      'Content-Type': 'video/mp4',
-    };
+    // if (req.headers.range) {
+    //   const parts = req.headers.range.replace(/bytes=/, '').split('-');
+    //   const start = parseInt(parts[0], 10);
+    //   const end = parts[1] ? parseInt(parts[1], 10) : file.length - 1;
+    //   return file.createReadStream({start, end}).pipe(res);
+    // }
+    //
+    // return file.createReadStream().pipe(res);
 
-    if (req.headers.range) {
-      const parts = req.headers.range.replace(/bytes=/, '').split('-');
-      const start = parseInt(parts[0], 10);
-      const end = parts[1] ? parseInt(parts[1], 10) : file.length - 1;
-      const chunkSize = (end - start) + 1;
-      headers['Content-Range'] = `bytes ${start}-${end}/${file.length}`;
-      headers['Accept-Ranges'] = 'bytes';
-      headers['Content-Length'] = chunkSize;
-    } else {
-      headers['Content-Length'] = file.length;
+    let range = req.headers.range;
+    range = range && rangeParser(file.length, range)[0];
+    res.setHeader('Accept-Ranges', 'bytes');
+    res.setHeader('Content-Type', 'video/mp4');
+    res.setHeader('transferMode.dlna.org', 'Streaming');
+    res.setHeader('contentFeatures.dlna.org', 'DLNA.ORG_OP=01;DLNA.ORG_CI=0;DLNA.ORG_FLAGS=01700000000000000000000000000000');
+    if (!range) {
+      res.setHeader('Content-Length', file.length);
+      if (req.method === 'HEAD') {
+        return res.end();
+      }
+      pump(file.createReadStream(), res);
+      return;
     }
 
-    res.writeHead(200, headers);
-    return file.createReadStream().pipe(res);
+    res.statusCode = 206;
+    res.setHeader('Content-Length', range.end - range.start + 1);
+    res.setHeader('Content-Range', 'bytes ' + range.start + '-' + range.end + '/' + file.length);
+    if (req.method === 'HEAD') return res.end();
+    pump(file.createReadStream(range), res);
   });
 };
